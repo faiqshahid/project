1be50d9a5ea090319aa7544d64a5cbcd95d16880
faiqshahid <faiqshahid846@gmail.com>
2024-12-24 18:49:23 +0500

    Add files via upload

---
+++ tables/app.js
@@ -0,0 +1,27 @@
+document.write('Result');
+document.write('<br>');
+document.write('The Value is =10');
+document.write('<br>');
+document.write('<p>. . . . . . . . . . . . . .</p>');
+document.write('<br>');
+document.write('The Value of ++a is:11');
+document.write('<br>');
+document.write('Now The Value of a is :11');
+document.write('<br>');
+document.write('<br>');
+document.write('the Value of a++ is:11');
+document.write('<br>');
+document.write('Now The Value of a is =12');
+document.write('<br>');
+document.write('<br>');
+document.write('the value of --a is:11');
+document.write('<br>');
+document.write('Now The Value of a is =10');
+document.write('<br>');
+document.write('<br>');
+document.write('The Value of a-- is:11');
+document.write('<br>');
+document.write('Now The Value of a is =10');
+
+
+
